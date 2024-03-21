9fb5157e80d9d153b72e33bd891d6f194612d15b
saroarlene <saroarlene@gmail.com>
2024-03-21 16:24:55 +0200

    subscribe component

--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import Subscribe from '../components/subscribe';
 
 export default function LandingPage(){
     return (
       <>
+      <Subscribe/>
         <Footer />
+        
        </>
     )
 }
