f043b937072a9a8061246a65a7bda8639bb892b6
chrisumurerwa <chrisumurerwa1@gmail.com>
2024-03-24 12:55:08 +0200

    (feat) home slider

--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import Categoriescard from '../components/Categoriescard';
 
 export default function LandingPage(){
     return (
       <>
-        <Footer />
+        <Categoriescard />
+        {/* <Footer /> */}
        </>
     )
 }
