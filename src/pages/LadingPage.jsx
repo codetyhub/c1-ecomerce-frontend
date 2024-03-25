b5cb0283bb62bcb1d3e4809417657287feccda49
fabricemu <fabthemachine@gmail.com>
2024-03-25 19:51:04 +0200

    Merge with Chris

--- src/pages/LadingPage.jsx
+++ src/pages/LadingPage.jsx
@@ -4,14 +4,15 @@ import OfferingCard from "../components/Offering";
 import Categoriescard from "../components/Categoriescard";
 import ExtraServices from "../components/ExtraServices.jsx";
 import SuppliersRegion from "../components/SuppliersRegion.jsx";
-export default function LandingPage(){
+
+export default function LandingPage() {
     return (
-      <>
-          <Categoriescard />
-        <OfferingCard />
-          <ExtraServices/>
+        <>
+            <Categoriescard/>
+            <OfferingCard/>
+            <ExtraServices/>
             <SuppliersRegion/>
-        <Footer />
-       </>
+            <Footer/>
+        </>
     )
 }
