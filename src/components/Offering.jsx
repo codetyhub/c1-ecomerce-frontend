a6b4ec73be47be3d5700ae7e078b1a57c9fd1329
fabricemu <fabthemachine@gmail.com>
2024-03-26 11:26:53 +0200

    removing blank space under offering cards

--- src/components/Offering.jsx
+++ src/components/Offering.jsx
@@ -8,7 +8,7 @@ import tu from "../assets/images/tu.svg";
 
 export default function OfferingCard() {
   return (
-    <div className="min-h-screen bg-white-100 p-6 ">
+    <div className="bg-white-100 p-6">
       <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6">
           <div className="bg-white p-4 flex justify-center items-center">
