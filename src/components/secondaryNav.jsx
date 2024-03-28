34c2bafddcc578046d28a4064f08644aed2f7714
saroarlene <saroarlene@gmail.com>
2024-03-28 11:23:52 +0200

    navigation bars

--- src/components/secondaryNav.jsx
+++ src/components/secondaryNav.jsx
@@ -1,14 +1,17 @@
 
 import React from 'react';
+import { IoMenuSharp } from "react-icons/io5";
+import { FaAngleDown } from "react-icons/fa";
+import flag from '../assets/images/flag.svg'
 
-const SecondaryNavigation = () => {
+const SecondaryNav = () => {
   return (
     <div className="flex justify-between px-12 py-3  shadow-sm hidden lg:flex">
       {/* first div */}
-      <div className="flex gap-8 text-semibold">
+      <div className="flex gap-4 text-semibold">
         {/* menu icon */}
-        <div>
-          <img src="../public/menu.svg" alt="" />
+        <div >
+         <IoMenuSharp size={20}/>
         </div>
         {/* All category*/}
         <div className="text-black font-semibold">All Category</div>
@@ -24,29 +27,28 @@ const SecondaryNavigation = () => {
         <div className="flex">
           <div className="text-black font-semibold">help</div>
           <div>
-            <img src="../public/expand_more.svg" />
+          <FaAngleDown/>
           </div>
         </div>
       </div>
       {/* second div */}
-      <div className="flex gap-8">
+      <div className="flex gap-6">
         {/* English usd */}
         <div className="flex">
           <h1 className="text-black font-semibold">English , USD</h1>
 
           <div>
-            <img src="../public/expand_more.svg" />
+           <FaAngleDown/>
           </div>
         </div>
         {/* ship to */}
-        <div className="flex gap-2 ">
-          <h1 className="text-black font-semibold">Ship 2</h1>
-          <img src="../public/flag.svg" alt="" />
-          <img src="../public/expand_more.svg" />
+        <div className="flex gap-1 ">
+          <h1 className="text-black font-semibold">Ship to</h1>
+          <img src={flag}/>
         </div>
       </div>
     </div>
   );
 };
 
-export default SecondaryNavigation;
+export default SecondaryNav;
