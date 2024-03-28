f450ba99a21ec9b591a627bb04ea7415cc24f779
saroarlene <saroarlene@gmail.com>
2024-03-28 11:39:58 +0200

    Responsive Navigation Bars

--- src/components/primaryNav.jsx
+++ src/components/primaryNav.jsx
@@ -1,44 +1,41 @@
 import React from 'react';
-import { MdMessage } from "react-icons/md";
-import { MdAccountBox } from "react-icons/md";
+import { MdMessage, MdAccountBox } from "react-icons/md";
 import { IoMdCart } from "react-icons/io";
 import { FaHeart } from "react-icons/fa";
 import logocolored from '../assets/images/logocolored.svg';
 
-
-
 const PrimaryNav = () => {
   return (
-    <div className="flex justify-between px-3 lg:px-12 py-3  shadow-sm relative">
+    <div className="flex justify-between px-3 lg:px-12 py-3 shadow-sm relative">
       {/* Logo container */}
       <div>
-        <img src={logocolored}/>
+        <img src={logocolored} alt="Logo" />
       </div>
-      {/* div containing the search , select and the search button */}
-      <div className="flex w-fit hidden lg:flex ">
-        {/* search  */}
+      {/* Search, Filter, and Search Button */}
+      <div className="flex flex-col lg:flex-row items-center space-y-2 lg:space-y-0 lg:space-x-4">
+        {/* Search */}
         <div>
           <input
             type="search"
             placeholder="Search"
-            className="py-2 px-3 outline-none w-80 h-10"
+            className="py-2 px-3 outline-none w-full lg:w-80 h-10"
             style={{
               border: 'solid 2px rgb(60, 131, 255)',
-              borderRadius: ' 12px 0 0 12px',
+              borderRadius: '12px',
             }}
           />
         </div>
-        {/* options to filter */}
+        {/* Filter */}
         <div>
           <select
             name="categories"
             id="categories"
-            className="py-2 w-72 border-blue h-10"
+            className="py-2 w-full lg:w-72 border-blue h-10"
             style={{
               border: 'solid 2px rgb(60, 131, 255)',
             }}
           >
-            <option  value="All category">All Category</option>
+            <option value="All category">All Category</option>
             <option value="">Lorem</option>
             <option value="">Lorem</option>
             <option value="">Lorem</option>
@@ -46,46 +43,34 @@ const PrimaryNav = () => {
             <option value="">Lorem</option>
           </select>
         </div>
+        {/* Search Button */}
         <div>
-          <button className="text-white bg-blue-500 py-2.0 rounded-r-md px-3 text-semi-bold h-10">
+          <button className="text-white bg-blue-500 py-2 rounded-r-md px-3 text-semi-bold h-10">
             Search
           </button>
         </div>
       </div>
-
-      {/* the links and profiles */}
-      <div className=" flex gap-4 hidden lg:flex">
-        <div className="justify-center">
-          <div className="flex justify-center">
-            {' '}
-            <MdAccountBox className=" text-gray-500 bg-white" size={20}/>
-          </div>
+      {/* Links and Profiles */}
+      <div className="flex gap-4 hidden lg:flex items-center">
+        <div className="justify-center flex flex-col items-center">
+          <MdAccountBox className="text-gray-500 bg-white" size={20}/>
           <h1 className="text-gray-400">Profile</h1>
         </div>
-        <div>
-          <div className="flex justify-center">
-            <MdMessage className=" text-gray-500 bg-white" size={20}/>
-          </div>
-
+        <div className="justify-center flex flex-col items-center">
+          <MdMessage className="text-gray-500 bg-white" size={20}/>
           <h1 className="text-gray-400">Message</h1>
         </div>
-        <div>
-          <div className="flex justify-center">
-            <FaHeart className=" text-gray-500 bg-white" size={20}/>
-          </div>
-
+        <div className="justify-center flex flex-col items-center">
+          <FaHeart className="text-gray-500 bg-white" size={20}/>
           <h1 className="text-gray-400">Orders</h1>
         </div>
-        <div>
-          <div className="flex justify-center">
-            <IoMdCart className=" text-gray-500 bg-white" size={20} />
-          </div>
-
+        <div className="justify-center flex flex-col items-center">
+          <IoMdCart className="text-gray-500 bg-white" size={20} />
           <h1 className="text-gray-400">My Cart</h1>
-         </div>
+        </div>
       </div>
-      </div>
+    </div>
   );
-          };
+};
 
 export default PrimaryNav;
--- src/components/secondaryNav.jsx
+++ src/components/secondaryNav.jsx
@@ -1,50 +1,42 @@
-
 import React from 'react';
 import { IoMenuSharp } from "react-icons/io5";
 import { FaAngleDown } from "react-icons/fa";
-import flag from '../assets/images/flag.svg'
+import flag from '../assets/images/flag.svg';
 
 const SecondaryNav = () => {
   return (
-    <div className="flex justify-between px-12 py-3  shadow-sm hidden lg:flex">
-      {/* first div */}
-      <div className="flex gap-4 text-semibold">
-        {/* menu icon */}
-        <div >
-         <IoMenuSharp size={20}/>
-        </div>
-        {/* All category*/}
-        <div className="text-black font-semibold">All Category</div>
+    <div className="flex justify-between px-4 lg:px-12 py-3 shadow-sm">
+      {/* Left side */}
+      <div className="flex items-center gap-4 text-semibold">
+        {/* Menu icon */}
+        <IoMenuSharp size={20} />
+        {/* All category */}
+        <div className="text-black font-semibold hidden lg:block">All Category</div>
         {/* Hot offers */}
-        <div className="text-black font-semibold">Hot Offers</div>
+        <div className="text-black font-semibold hidden lg:block">Hot Offers</div>
         {/* Gift Boxes */}
-        <div className="text-black font-semibold">GiftBoxes</div>
+        <div className="text-black font-semibold hidden lg:block">GiftBoxes</div>
         {/* Projects */}
-        <div className="text-black font-semibold">Projects</div>
-        {/* MenuItem */}
-        <div className="text-black font-semibold">Menu Item</div>
-        {/* Help*/}
-        <div className="flex">
-          <div className="text-black font-semibold">help</div>
-          <div>
-          <FaAngleDown/>
-          </div>
+        <div className="text-black font-semibold hidden lg:block">Projects</div>
+        {/* Menu Item */}
+        <div className="text-black font-semibold hidden lg:block">Menu Item</div>
+        {/* Help */}
+        <div className="flex items-center gap-1 text-black font-semibold">
+          help
+          <FaAngleDown />
         </div>
       </div>
-      {/* second div */}
-      <div className="flex gap-6">
-        {/* English usd */}
-        <div className="flex">
-          <h1 className="text-black font-semibold">English , USD</h1>
-
-          <div>
-           <FaAngleDown/>
-          </div>
+      {/* Right side */}
+      <div className="flex items-center gap-4">
+        {/* English & Currency */}
+        <div className="flex items-center gap-1 text-black font-semibold">
+          English , USD
+          <FaAngleDown />
         </div>
-        {/* ship to */}
-        <div className="flex gap-1 ">
+        {/* Ship to */}
+        <div className="flex items-center gap-1">
           <h1 className="text-black font-semibold">Ship to</h1>
-          <img src={flag}/>
+          <img src={flag} alt="Flag" />
         </div>
       </div>
     </div>
