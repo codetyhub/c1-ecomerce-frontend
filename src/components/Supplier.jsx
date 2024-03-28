6f1eef585c7293f4618ee72370f6699103206620
fabricemu <fabthemachine@gmail.com>
2024-03-29 00:07:36 +0200

    adding font size

--- src/components/Supplier.jsx
+++ src/components/Supplier.jsx
@@ -2,9 +2,9 @@ import "./App.css";
 function Supplier() {
     return (
         <>
-            <div className="container px-[50px] mx-auto backgroundImage lg:flex-row sm:flex-col flex-col items-center flex justify-between gap-10 py-7 flex-wrap bg-cover">
+            <div className="container px-[50px] mx-auto backgroundImage lg:flex-row sm:flex-col flex-col flex justify-between gap-10 py-7 flex-wrap bg-cover">
                 <div className='lg:w-[40%] w-full'>
-                    <h2 className="text-lg text-4xl text-white">An easy way to send requests to all suppliers</h2>
+                    <h2 className="text-lg text-4xl text-white font-bold text-3xl">An easy way to send <br /> requests to all suppliers</h2>
                     <p className="text-sm mt-4 text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
                 </div>
                 <div className=' bg-white lg:w-[40%] w-full rounded-md py-7 px-10 text-black'>
