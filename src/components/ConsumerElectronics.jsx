016c07eb08a83c6289117e9ed3a023d36f6f7d8b
pascalniri <pascalniri@gmail.com>
2024-04-01 02:51:50 -0500

    Changed the images

--- src/components/ConsumerElectronics.jsx
+++ src/components/ConsumerElectronics.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import img1 from "../assets/images/image1.svg";
-import img2 from "../assets/images/image2.svg";
-import img3 from "../assets/images/image3.svg";
-import img4 from "../assets/images/image4.svg";
-import img5 from "../assets/images/image5.svg";
-import img6 from "../assets/images/image6.svg";
-import img7 from "../assets/images/image7.svg";
-import img8 from "../assets/images/image8.svg";
-import img9 from "../assets/images/image9.svg";
+import img1 from "../assets/images/img1.svg";
+import img2 from "../assets/images/img2.svg";
+import img3 from "../assets/images/img3.svg";
+import img4 from "../assets/images/img4.svg";
+import img5 from "../assets/images/img5.svg";
+import img6 from "../assets/images/img6.svg";
+import img7 from "../assets/images/img7.svg";
+import img8 from "../assets/images/img8.svg";
+import img9 from "../assets/images/img9.svg";
 
 const ConsumerElectronics = () => {
     return (
         <div className=''>
             <div className="flex container lg:flex-row sm:flex-col md:flex-col flex-col mx-auto p-10">
                 <div className="lg:w-[500px] md:w-full sm:w-full  p-5 object-fill bg-cover bg-center" style={{backgroundImage: `url(${img9})`}}>
-                    <h3 className="text-black font-bold text-[24px] w-[50%] mb-4">Home and outdoor</h3>
+                    <h3 className="text-black font-bold text-[24px] w-[50%] mb-4">Consumer electronics and gadgets</h3>
                     <button className="bg-white hover:bg-gray-400 transition text-black text-[20px] font-[500]  px-4 py-2 mt-[10px] rounded-md">
                         Source now
                     </button>
@@ -24,7 +24,7 @@ const ConsumerElectronics = () => {
                     {/* Single Box */}
                     <div className="flex justify-between p-[10px] mx-[5px] cursor-pointer">
                         <div className='text-[13px]'>
-                            <p className="text-[16px]">Soft chairs</p>
+                            <p className="text-[16px]">Smart watches</p>
                             <p className='text-gray-500'>From<br/>USD 19</p>
                         </div>
                         <div className="pt-[30px]">
@@ -34,7 +34,7 @@ const ConsumerElectronics = () => {
 
                     <div className="flex justify-between p-[10px]  mx-[5px]  cursor-pointer">
                         <div className="text-[13px]">
-                            <p className="text-[16px]">Soft & chairs</p>
+                            <p className="text-[16px]">Cameras</p>
                             <p className='text-gray-500'>From<br/>USD 19</p>
                         </div>
                         <div className="pt-[30px]">
@@ -44,7 +44,7 @@ const ConsumerElectronics = () => {
 
                     <div className="flex justify-between p-[10px]  mx-[5px]  cursor-pointer">
                         <div className="text-[13px]">
-                            <p className="text-[16px]">Kitchen dishes</p>
+                            <p className="text-[16px]">Headphones</p>
                             <p className='text-gray-500'>From<br/>USD 19</p>
                         </div>
                         <div className="pt-[30px]">
@@ -64,7 +64,7 @@ const ConsumerElectronics = () => {
 
                     <div className="flex justify-between p-[10px]  mx-[5px]  cursor-pointer">
                         <div className="text-[13px]">
-                            <p className="text-[16px]">Kitchen mixer</p>
+                            <p className="text-[16px]">Gaming set</p>
                             <p className='text-gray-500'>From<br/>USD 19</p>
                         </div>
                         <div className="pt-[30px]">
@@ -74,7 +74,7 @@ const ConsumerElectronics = () => {
 
                     <div className="flex justify-between p-[10px]  mx-[5px]  cursor-pointer">
                         <div className="text-[13px]">
-                            <p className="text-[16px]">Blenders</p>
+                            <p className="text-[16px]">Laptops & PC</p>
                             <p className='text-gray-500'>From<br/>USD 19</p>
                         </div>
                         <div className="pt-[30px]">
@@ -84,7 +84,7 @@ const ConsumerElectronics = () => {
 
                     <div className="flex justify-between p-[10px] mx-[5px] cursor-pointer">
                         <div className="text-[13px]">
-                            <p className="text-[16px]">Home appliance</p>
+                            <p className="text-[16px]">Smartphones</p>
                             <p className='text-gray-500'>From<br/>USD 19</p>
                         </div>
                         <div className="pt-[30px]">
@@ -94,7 +94,7 @@ const ConsumerElectronics = () => {
 
                     <div className="flex justify-between p-[10px]  mx-[5px]  cursor-pointer">
                         <div className="text-[13px]">
-                            <p className="text-[16px]">Coffee maker</p>
+                            <p className="text-[16px]">Telephones</p>
                             <p className='text-gray-500'>From<br/>USD 19</p>
                         </div>
                         <div className="pt-[30px]">
