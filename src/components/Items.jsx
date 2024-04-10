6e2e79c0905434e0875ffd913d37ed101f9e037a
Sandrine <ingabiremariesandrine187>
2024-04-10 22:42:50 +0200

    items and leftside bar

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -10,59 +10,63 @@ function Items() {
   return (
     <div className="border border-gray-200 rounded p-4">
       <div className="text-black text-md font-bold">Related Products</div>
-      <div className="flex gap-2">
-        <div className="items-center">
-          <img src={FirstPicture} alt="Image 1" className="w-20 h-20 rounded-lg" />
-          <div>
-            <div className="text-black text-small">Xiaomi Redmi 8</div>
-            <div className="text-gray text-small">Original</div>
-            <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
-          </div>
+      <div className="border border-gray-200 rounded p-4"> <div className="flex gap-2">
+
+<div className="items-center">
+  <img src={FirstPicture} alt="Image 1" className="w-20 h-20 rounded-lg" />
+  <div>
+    <div className="text-black text-small">Xiaomi Redmi 8</div>
+    <div className="text-gray text-small">Original</div>
+    <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
+  </div>
+</div>
+<div>
+  <img src={SecondPicture} alt="Image 2" className="w-20 h-20 rounded-lg" />
+  <div>
+    <div className="text-black text-small">Xiaomi Redmi 8</div>
+    <div className="text-gray text-small">Original</div>
+    <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
+  </div>
+</div>
+<div>
+  <img src={ThirdPicture} alt="Image 3" className="w-20 h-20 rounded-lg" />
+  <div>
+    <div className="text-black text-small">Xiaomi Redmi 8</div>
+    <div className="text-gray text-small">Original</div>
+    <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
+  </div>
+</div>
+<div>
+  <img src={ForthPicture} alt="Image 4" className="w-20 h-20 rounded-lg" />
+  <div>
+    <div className="text-black text-small">Xiaomi Redmi 8</div>
+    <div className="text-gray text-small">Original</div>
+    <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
+  </div>
+</div>
+<div>
+  <img src={FifthPicture} alt="Image 5" className="w-20 h-20 rounded-lg" />
+  <div>
+    <div className="text-black text-small">Xiaomi Redmi 8</div>
+    <div className="text-gray text-small">Original</div>
+    <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
+  </div>
+</div>
+<div>
+  <img src={SixthPicture} alt="Image 6" className="w-20 h-20 rounded-lg" />
+  <div>
+    <div className="text-black text-small">Xiaomi Redmi 8</div>
+    <div className="text-gray text-small opacity 0.5">Original</div>
+    <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
+  </div>
+</div>
+</div></div>
+<div className="bg-blue-500 border border-blue-500 mt-4 h-20 flex flex-row justify-between px-8" style={{borderRadius: '3px'}}>    
+       <div className="flex flex-col"> <span className="text-white text-medium font-bold ">Super Discount on more than $100 USD</span>
+        <span className="text-white text-extra-small">Have you ever finally just written Dummy info?</span></div>
+        <div className="bg-yellow-500 border border-yellow-500 mt-2 flex flex-row" style={{ width: '75px', height: '30px', borderRadius: '3px', marginLeft: '80px' ,margitop: '5' }}>
+        <div className="text-white text-bold text-small" style={{padding: '5px'}}>Shopnow</div>        
         </div>
-        <div>
-          <img src={SecondPicture} alt="Image 2" className="w-20 h-20 rounded-lg" />
-          <div>
-            <div className="text-black text-small">Xiaomi Redmi 8</div>
-            <div className="text-gray text-small">Original</div>
-            <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
-          </div>
-        </div>
-        <div>
-          <img src={ThirdPicture} alt="Image 3" className="w-20 h-20 rounded-lg" />
-          <div>
-            <div className="text-black text-small">Xiaomi Redmi 8</div>
-            <div className="text-gray text-small">Original</div>
-            <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
-          </div>
-        </div>
-        <div>
-          <img src={ForthPicture} alt="Image 4" className="w-20 h-20 rounded-lg" />
-          <div>
-            <div className="text-black text-small">Xiaomi Redmi 8</div>
-            <div className="text-gray text-small">Original</div>
-            <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
-          </div>
-        </div>
-        <div>
-          <img src={FifthPicture} alt="Image 5" className="w-20 h-20 rounded-lg" />
-          <div>
-            <div className="text-black text-small">Xiaomi Redmi 8</div>
-            <div className="text-gray text-small">Original</div>
-            <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
-          </div>
-        </div>
-        <div>
-          <img src={SixthPicture} alt="Image 6" className="w-20 h-20 rounded-lg" />
-          <div>
-            <div className="text-black text-small">Xiaomi Redmi 8</div>
-            <div className="text-gray text-small opacity 0.5">Original</div>
-            <div className="text-gray text-small" style={{ opacity: 0.5}}>$32.00-$40.00</div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-blue-500 border border-blue-500 mt-4 h-18">
-        <div className="text-white text-medium font-bold">Super Discount on more than $100 USD</div>
-        <div className="text-white text-small">Have you ever finally just written Dummy info?</div>
       </div>
     </div>
   );
