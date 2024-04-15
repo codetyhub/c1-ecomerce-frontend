d3e1e456b84b13678907dc77e8666316ac6835bf
saroarlene <saroarlene@gmail.com>
2024-04-15 18:41:55 +0200

    saved for later component

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import MyCart from "../components/MyCart";
+import SavedForLater from "../components/SavedForLater";
+import PrimaryNav from "../components/PrimaryNav.jsx";
+import SecondaryNav from "../components/SecondaryNav.jsx";
+import Footer from '../components/Footer';
 
 export default function CartPage() {
     return(
         <>
+            <PrimaryNav />
+            <SecondaryNav />
             <MyCart />
+            <SavedForLater/>
+            <Footer/>
         </>
     )
 }
